146fa42c13432fbc19ea57cc9a5b834aabf2e8e3
LongEzLand <martinpaju@gmail.com>
2017-10-23 14:54:23 +0300

    liitmine

---
+++ src/01-exercises/exercise 01/ConsoleApplication4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mina olen kalkulaatori programm. Ütle mulle 2 numbrit ja ma ütlen sulle nende summa");
+            Console.WriteLine();
+            Console.WriteLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine("Esimene number: " + firstNumber);
+            Console.WriteLine("Teine number: " + secondNumber);
+            Console.WriteLine("Summa on " + sum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
