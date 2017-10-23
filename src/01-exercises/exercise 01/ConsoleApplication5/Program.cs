f80b6e7b8b3d5802d7725124b5d0b2994c480bf7
LongEzLand <martinpaju@gmail.com>
2017-10-23 15:12:33 +0300

    numbrimäng

---
+++ src/01-exercises/exercise 01/ConsoleApplication5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 23;
+            Console.WriteLine("Ma valin välja ühe suvalise numbri [1-100]. Proovi see ära arvata :)");
+            Console.Write("Sina pakud ");
+            int userGuess = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sina pakkusid: " + userGuess);
+            if (userGuess > number) {
+                Console.WriteLine("Minu valitud number on väiksem");
+            }
+            if (userGuess < number)
+            {
+                Console.WriteLine("Minu valitud number on suurem");
+            }
+            if (userGuess == number)
+            {
+                Console.WriteLine("Arvasid ära!");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
