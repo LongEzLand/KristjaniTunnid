226bf423bee1955485f98f0221b7aed53d7c7bb7
LongEzLand <martinpaju@gmail.com>
2017-10-25 13:45:52 +0300

    itroduced for cycle

--- src/03-dice-roller/DiceRoller/Program.cs
+++ src/03-dice-roller/DiceRoller/Program.cs
@@ -10,17 +10,35 @@ namespace DiceRoller
     {
         static void Main(string[] args)
         {
-            var rnGenD6 = new Random().Next(1, 6);
-            var rnGenD8 = new Random().Next(1, 8);
-
+            
             Console.WriteLine("> /roll 3d6 2d8");
             Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("1d6: " + rnGenD6);
-            Console.WriteLine("1d6: " + rnGenD6);
-            Console.WriteLine("1d6: " + rnGenD6);
-            Console.WriteLine("1d8: " + rnGenD8);
-            Console.WriteLine("1d6: " + rnGenD8);
+
+            var rnGenD6 = new Random();
+            var rnGenD8 = new Random();
+
+            var random = new Random();
+            var total = 0;
+
+            for (var i=0; i<3; i++)
+            {
+                var roll = random.Next(1, 7);
+                total += roll;
+
+                Console.WriteLine($"1d6: { roll }");
+            }
+
+            
+
+            for (var i = 0; i < 2; i++)
+            {
+                var roll = random.Next(1, 9);
+                total += roll;
+
+                Console.WriteLine($"1d8: { roll }");
+            }
+
+            
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Roll total: 20");
